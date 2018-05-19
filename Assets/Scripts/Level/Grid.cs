a69cbaa07e983bab77396f448c871910b6794d3a
InstaPizza <ovanzanten@gmail.com>
2018-05-19 22:41:18 +0200

    Default stuff

--- Assets/Scripts/Level/Grid.cs
+++ Assets/Scripts/Level/Grid.cs
@@ -16,27 +16,43 @@ public class Grid : MonoBehaviour
     {
         plots = new Plot[diameter * diameter * diameter];
 
-        for (int x = -diameter/2; x < diameter/2; x++)
+        for (int x = -diameter / 2; x < diameter / 2; x++)
         {
-            for (int y = -diameter/2; y < diameter/2; y++)
+            for (int y = -diameter / 2; y < diameter / 2; y++)
             {
-                for (int z = -diameter / 2; z < diameter/2; z++)
+                for (int z = -diameter / 2; z < diameter / 2; z++)
                 {
-                    if ((x + y + z)+1 != 0) continue;
-
-                    GameObject obj = Instantiate(plotPrefab);
-                    obj.transform.parent = terrain;
-                    obj.transform.position = new Vector3(x, y, z);
-
-                   // plots[x + (y * radius) + (z * radius * radius)] = obj.GetComponent<Plot>();
+                    if ((x + y + z + 1) == 0)
+                    {
+                        GameObject obj = Instantiate(plotPrefab);
+                        obj.transform.parent = terrain;
+                        obj.transform.position = new Vector3(x * Mathf.Sqrt(2) ,0, z * Mathf.Sqrt(2));
+                        obj.transform.rotation = Quaternion.Euler(45, 45, 45);
+                    }
+                    // plots[x + (y * radius) + (z * radius * radius)] = obj.GetComponent<Plot>();
                 }
             }
         }
     }
 
+
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public Vector2 CubeToAxial(Vector3 cube)
+    {
+        var x = cube.x;
+        var y = cube.z;
+        return new Vector2(x, y);
+    }
+
+    public Vector3 AxialToCube(Vector2 hex) {
+        var x = hex.x;
+        var y = hex.y;
+        var z = -x - y;
+        return new Vector3(x, y, z);
     }
 }
