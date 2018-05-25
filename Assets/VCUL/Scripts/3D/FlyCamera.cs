1674d8c90620a5f190e732f99a0caa162b9d3bc5
InstaPizza <ovanzanten@gmail.com>
2018-05-25 20:26:44 +0200

    Camera rotation update

--- Assets/VCUL/Scripts/3D/FlyCamera.cs
+++ Assets/VCUL/Scripts/3D/FlyCamera.cs
@@ -11,6 +11,8 @@ public class FlyCamera : MonoBehaviour
     public float maxHeight = 100;
     public float groundOFfset = 10;
     public Vector2 rotationLimitVertical;
+    public float HeightOffset = 30;
+    public float MaxAngle = 45;
 
     private float speedScale { get { return transform.position.y / maxHeight; } }
     public Transform Camera;
@@ -22,9 +24,9 @@ public class FlyCamera : MonoBehaviour
     }
 
     // LateUpdate is called every frame, if the Behaviour is enabled
-    private void Update()
+    private void FixedUpdate()
     {
-       
+
         float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
@@ -32,11 +34,10 @@ public class FlyCamera : MonoBehaviour
         {
 
             transform.Rotate(Vector3.up, x, Space.Self);
-
-            float angle = Mathf.Clamp(Camera.transform.rotation.eulerAngles.x - y, rotationLimitVertical.x, rotationLimitVertical.y);
-
-            Camera.transform.localRotation = Quaternion.Euler(angle, 0, 0);
         }
+        float angle = transform.position.y > HeightOffset ? 90 : MaxAngle+ (Mathf.Min(HeightOffset, transform.position.y) / HeightOffset * (90- MaxAngle));
+        Camera.transform.localRotation = Quaternion.Euler(angle, 0, 0);
+
 
         transform.position += transform.right * (normalMoveSpeed * speedScale) * Input.GetAxis("Horizontal") * Time.deltaTime;
         transform.position += transform.forward * (normalMoveSpeed * speedScale) * Input.GetAxis("Vertical") * Time.deltaTime;
